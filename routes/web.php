bb61b6cdd41b1cf2835fdf5ee588a788f17517dc
aurelioo29 <aureliolucio5@gmail.com>
2025-11-06 13:33:08 +0700

    feat: update dashboard views with chart js integrate

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::view('/dashboard', 'dashboard')->name('dashboard');
+    Route::get('/dashboard', DashboardController::class)->name('dashboard');
 
     Route::prefix('dashboard')->name('dashboard.')->group(function () {
         // LIST (tabel saja)

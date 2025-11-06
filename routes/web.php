2fb0c07048a7312525425c3706ad2e5c4146267b
aurelioo29 <aureliolucio5@gmail.com>
2025-11-06 12:25:44 +0700

    feat: add routes post

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+
+    Route::prefix('dashboard')->name('dashboard.')->group(function () {
+        // LIST (tabel saja)
+        Route::view('/posts', 'posts.index')->name('posts.index');
+
+        // CREATE (form)
+        Route::view('/posts/create', 'posts.create')->name('posts.create');
+
+        // EDIT (form) — pakai route parameter {post}
+        Route::view('/posts/{post}/edit', 'posts.edit')->name('posts.edit')
+            ->whereNumber('post');
+    });
+});
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
